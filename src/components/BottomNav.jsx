b80dc862f9c1a7d2d018a6dc0f2c5388faf14743
Ron-Reagans_Ghost <alexander.singleton40@gmail.com>
2018-03-26 13:28:50 -0400

    Footer links and animated images draft

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-
+import { Grid, Row, Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
 
 class BottomNav extends Component {
   render() {
@@ -8,17 +9,41 @@ const bottomNav = {
     position: 'absolute',
     bottom: '60px',
     width: '100%',
-    height: '600px',
+    height: '300px',
     lineHeight: '60px',
     backgroundColor: '#f5f5f5',
+    flexDirection: 'flex-left'
+}
+
+const linkStyle = {
+  padding: '-0px',
 }
 
   return(
   <div style={bottomNav} className="footer">
-    <div className="container">
-      <span className="text-muted">AMRC</span>
-    </div>
+    <Grid>
+      <Row className="show-grid">
+        <Col sm={2} md={2}>
+          <h2> About </h2>
+          <Link className="nav-link" to="/" style={linkStyle}> The Lionfish Problem </Link>
+          <Link className="nav-link" to="/" style={linkStyle}> News </Link>
+          <Link className="nav-link" to="/sponsors" style={linkStyle}> Sponsors and Endorsments </Link>
+        </Col>
+        <Col sm={2} md={2}>
+          <h2> Resources </h2>
+          <Link className="nav-link" to="/research" style={linkStyle}> Ecology </Link>
+          <Link className="nav-link" to="/research" style={linkStyle}> Technology </Link>
+        </Col>
+        <Col sm={2} md={2}>
+          <h2> Get Involved </h2>
+          <Link className="nav-link" to="/sponsors" style={linkStyle}> Sponsor </Link>
+          <Link className="nav-link" to="/contact" style={linkStyle}> Careers </Link>
+          <Link className="nav-link" to="/contact" style={linkStyle}> Contact </Link>
+        </Col>
+      </Row>
+    </Grid>
   </div>
+
    );
   }
 }
