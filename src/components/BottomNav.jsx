0e50e7be3f18128fc9e62f35891054141c90d682
Ron-Reagans_Ghost <alexander.singleton40@gmail.com>
2018-03-27 14:36:39 -0400

    Animate and divs work together

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -6,7 +6,6 @@ class BottomNav extends Component {
   render() {
 
 const bottomNav = {
-    position: 'absolute',
     bottom: '60px',
     width: '100%',
     height: '300px',
--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -7,8 +7,6 @@ import BottomNav from './BottomNav.jsx';
 import Background from '../images/lionfish_1.jpg';
 import '../App.css';
 
-//import logo from './logo.svg';
-
 class Sponsors extends Component  {
   render(){
 
@@ -17,6 +15,20 @@ class Sponsors extends Component  {
       padding: '0px',
       height: '100vh',
       marginBottom: '0px',
+    }
+
+    const still = {
+      display: 'flex',
+      flex: '1 1 0%',
+      backgroundColor: 'rgba(0, 0, 0, 0.3)',
+
+      position: 'relative',
+      height: '100%',
+      width: '100%',
+      zIndex: '1',
+      backgroundSize: 'cover',
+      backgroundPosition: '50% 50%',
+      backgroundRepeat: 'no-repeat'
     }
 
     const inner = {
@@ -29,19 +41,113 @@ class Sponsors extends Component  {
     flexDirection: 'column',
     }
 
-    const still = {
-      display: 'flex',
+    const reveal = {
       flex: '1 1 0%',
-      backgroundColor: 'rgba(0, 0, 0, 0.3)',
+      display: 'flex',
+      justifyContent: 'space-between',
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+      paddingLeft: '8px',
+      paddingRight: '20vw',
+      opacity: '1',
+    }
+
+    const headline = {
+      wordBreak: 'keep-all',
+      fontSize: '10vw',
+      color: 'rgb(255, 255, 255)',
+      fontWeight: '600',
+      lineHeight: '94%',
+      marginBottom: '10px',
+      textAlign: 'start',
     }
 
     const image = {
-      backgroundImage: `url(${Background})`,
+      flex: '0 1 0%',
+      backgroundImage: `url(${Background})`
+    }
+
+    const underscore = {
+      marginTop: '5px',
+      minWidth: '88px',
+      minHeight: '5px',
+      maxWidth: '88px',
+      maxHeight: '5px',
+      flex: '0 1 0%',
+      display: 'flex',
+      backgroundColor: 'rgb(255, 255, 255)',
+    }
+
+    const bottomButton = {
+      flex: '0 1 0%',
+      display: 'flex',
+      width: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      opacity: '1',
+    }
+
+    const problem = {
+      backgroundColor: 'rgb(28, 51, 92)',
+      flex: '1 1 0%',
+      display: 'flex',
+      alignItems: 'center',
+      flexDirection: 'column',
+      justifyContent: 'flex-start',
+      width: '100vw',
+      paddingTop: '9vh',
+      paddingBottom: '5em',
+      opacity: '1',
+    }
+
+    const subtitle = {
+      wordBreak: 'keep-all',
+      overflowWrap: 'normal',
+      fontSize: '30px',
+      color: 'rgb(255, 255, 255)',
+      letterSpacing: '2px',
+      lineHeight: '110%',
+      fontWeight: '600',
+      marginBottom: '10px',
+      opacity: '1',
+    }
+
+    const paras = {
+      color: 'rgba(255, 255, 255, 0.4)',
+      fontSize: '20px',
+      marginTop: '30px',
+      fontFamily: 'Prompt',
     }
 
     return (
         <div>
-          <img src={Background} className="Moving-back" />
+          <NavBar />
+            <div className="jumbotron" style={jumbo}>
+              <div className="still" style={still}>
+                <div className="Moving-back" style={image}>
+                </div>
+                <div style={inner}>
+                  <div className="inner" style={reveal}>
+                    <div >
+                      <p style={headline}>INNOVATIONS FOR HEALTHY OCEANS</p>
+                        <div style={underscore}>
+                        </div>
+                    </div>
+                  </div>
+                  <div style={bottomButton}>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div id="problem" className="problem" style={problem}>
+              <div style={underscore}>
+              </div>
+              <br/>
+              <h1 style={subtitle}> THE LIONFISH PROBLEM </h1>
+              <p style={paras}> Lionfish need to be gotten rid of honestly </p>
+            </div>
+          <BottomNav />
+          <Footer />
         </div>
     );
   }
